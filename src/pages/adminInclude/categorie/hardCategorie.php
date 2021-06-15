78d9e34df7307336b00bdcf0cf0a0be53d535765
clement90 <clement_evrard@hotmail.com>
2021-06-15 21:40:19 +0200

    Projet Jeux Partie 3

--- src/pages/adminInclude/categorie/hardCategorie.php
+++ src/pages/adminInclude/categorie/hardCategorie.php
@@ -1,4 +1,21 @@
 <?php
+    //J conditionne l'accès à ces focntions aux seuls admins
+    if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "admin"){
+        //Gérer les ajouts de nouveau matériel
+        if(isset($_POST["hardware"]) && !empty($_POST["hardware"])){
+
+            $console = htmlspecialchars($_POST["hardware"]);
+            addHardCategorie($console);
+        }
+
+        //Effacer une console
+        if(isset($_GET["delete"]) && $_GET["delete"] == true){
+            $hardId = htmlspecialchars($_GET["value"]);
+            $intHardId = intval(($hardId));
+            deleteHardCategorie($intHardId);
+        }
+    }
+
     // Je récupère la liste catégorie
     $listeCategorie = getHardCategorie();
   
@@ -26,9 +43,32 @@
                 ?>
                     <tr>
                         <td><?= $value["console"]?></td>
-                        <td class="ta-c tc-r"><a href="../../src/pages/admin.php?choix=listeCategorie&delete=true&value=<?=$value["console"]?>"><i class="far fa-plus-square"></a></td>
+                        <td class="ta-c tc-r"><a href="../../src/pages/admin.php?choix=listeCategorie&delete=true&value=<?=$value["hardId"]?>"><i class="far fa-plus-square"></a></td>
                     </tr>
                 <?php
+            }
+        ?>
+        <tr>
+            <td>Ajouter un hardware</td>
+            <td class="ta-c tc-g"><a href="../../src/pages/admin.php?choix=listeCategorie&create=true"><i class="far fa-plus-square"></i></a></td>
+        </tr>
+
+        <?php
+        // Je conditionne l'accès aux fonction ci-dessous aux seuls administrateurs du site
+            if(isset($_SESSION["user"]["role"]) && $_SESSION["user"]["role"] == "admin"){
+                // Formulaire pour ajouter harware
+                if(isset($_GET["create"]) && $_GET["create"]==true){
+                    ?>
+                        <form action="../../src/pages/admin.php?choix=listeCategorie" method="post">  
+                            <tr>
+                                <td>Nom du hardware à ajouter</td>
+                                <td class="ta-c tc-g"><input type="text" name="hardware" placeholder="Entrez le nom du hard" required></td>
+                                <td><input type="submit" value="Ajouter hardware"></td>
+                            </tr>
+                        </form>
+
+                    <?php
+                }
             }
         ?>
     </tbody>
